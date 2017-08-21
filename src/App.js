d64b0ffc17f3496a47e460a39048982b143fa25d
John Son <jsson77@gmail.com>
2017-08-21 11:50:59 -0700

    abstract out todo input form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoForm } from './components/todo/TodoForm';
 
 class App extends Component {
+  constructor() {
+    super();  //  make sures that the constructor of the component class we're extending is called
+    this.state = {
+      todos: [
+        {id: 1, name: 'Learn JSX', isComplete: true},
+        {id: 2, name: 'Learn HTML', isComplete: false},
+        {id: 3, name: 'Ship it', isComplete: false},
+      ],
+      currentTodo: '',
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange (e) {
+    this.setState({
+      currentTodo: e.target.value,
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,14 +32,19 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
-          <form>
-            <input type="text"/>
-          </form>
+          <TodoForm handleInputChange={this.handleInputChange}
+            currentTodo={this.state.currentTodo}
+          />
           <div className="Todo-List">
             <ul>
-              <li><input type="checkbox"/>Learn JSX</li>
-              <li><input type="checkbox"/>Learn HTML</li>
-              <li><input type="checkbox"/>Ship it</li>
+              {
+                this.state.todos.map(todo => (
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete}/>
+                  {todo.name}
+                </li>
+                ))
+              }
             </ul>
           </div>
         </div>
