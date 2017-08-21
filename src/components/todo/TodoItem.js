b50ae04a28d2cdaecc2f1854bef24d8dcdc95f64
John Son <jsson77@gmail.com>
2017-08-21 12:07:09 -0700

    use spread operator to deconstruct a prop {...todos}, for use of props.id props.name instead of props.todo.id props.todo.name

---
+++ src/components/todo/TodoItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const TodoItem = (props) => (
+  <li>
+    <input type="checkbox" defaultChecked={props.isComplete}/>
+    {props.name}
+  </li>
+)
--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { TodoItem } from './TodoItem';
 
-export const TodoList = (props) => (
-  <div className="Todo-List">
-    <ul>
-      {
-        this.props.todos.map(todo => (
-        <li key={todo.id}>
-          <input type="checkbox" defaultChecked={todo.isComplete}/>
-          {todo.name}
-        </li>
-        ))
-      }
-    </ul>
-  </div>
-)
+export const TodoList = (props) => {
+    console.log('props:TodoList ', props);
+  return (
+    <div className="Todo-List">
+      <ul>
+        {
+          props.todos.map(todo => (
+            <TodoItem key={todo.id} {...todo} />
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+//
+// export const TodoList = (props) => (
+//   <div className="Todo-List">
+//     <ul>
+//       {
+//         props.todos.map(todo => (
+//           <TodoList todo={todo} />
+//         // <li key={todo.id}>
+//         //   <input type="checkbox" defaultChecked={todo.isComplete}/>
+//         //   {todo.name}
+//         // </li>
+//         ))
+//       }
+//     </ul>
+//   </div>
+// )
