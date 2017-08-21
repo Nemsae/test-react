1144a193c1e5fcfca99dd34b7f4e9b9dc06a6e50
John Son <jsson77@gmail.com>
2017-08-21 11:54:48 -0700

    single js file to import all views

---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const TodoList = (props) => (
+  <div className="Todo-List">
+    <ul>
+      {
+        this.props.todos.map(todo => (
+        <li key={todo.id}>
+          <input type="checkbox" defaultChecked={todo.isComplete}/>
+          {todo.name}
+        </li>
+        ))
+      }
+    </ul>
+  </div>
+)
